0d1a80e707a6d197f0c9dd8b2130effa45524fdd
Logan Miles <144288661+lcmiles@users.noreply.github.com>
2024-07-24 15:22:13 -0500

    rebuild db with next deployment

--- app.py
+++ app.py
@@ -284,6 +284,6 @@ def search_users():
 
 if __name__ == "__main__":
     with app.app_context():
-
+        db.drop_all()
         db.create_all()
     app.run(debug=True, host="0.0.0.0", port=8080)
